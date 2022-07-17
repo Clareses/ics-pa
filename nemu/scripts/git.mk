79dd317889d4cfbab62d01a2a3f3187ccc5b9a5c
Clareses <1315236833@qq.com>
2022-07-17 11:25:08 +0800

    关闭了tracer

--- nemu/scripts/git.mk
+++ nemu/scripts/git.mk
@@ -7,7 +7,11 @@ GITFLAGS = -q --author='tracer-ics2021 <tracer@njuics.org>' --no-verify --allow-
 
 # prototype: git_commit(msg)
 define git_commit
-	-@git add $(NEMU_HOME)/.. -A --ignore-errors
+	# -@git add $(NEMU_HOME)/.. -A --ignore-errors
+	# -@while (test -e .git/index.lock); do sleep 0.1; done
+	# -@(echo "> $(1)" && echo $(STUID) && hostnamectl && uptime) | git commit -F - $(GITFLAGS)
+	# -@sync
+	-@git add .. -A --ignore-errors
 	-@while (test -e .git/index.lock); do sleep 0.1; done
 	-@(echo "> $(1)" && echo $(STUID) && hostnamectl && uptime) | git commit -F - $(GITFLAGS)
 	-@sync
